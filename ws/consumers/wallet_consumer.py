c109cf2d8f2d8d7f6916afef3481152a757be673
longIdt2502 <long.lbh@idtinc.co>
2024-12-21 14:54:30 +0700

    hook zalo: update logic check

--- ws/consumers/wallet_consumer.py
+++ ws/consumers/wallet_consumer.py
@@ -33,16 +33,23 @@ class WalletConsumer(WebsocketConsumer):
         )
 
     def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
         event = {
             'type': 'message_handler',
+            'message': message
         }
         async_to_sync(self.channel_layer.group_send)(
             f'wallet_{self.wallet_id}', event
         )
 
     def message_handler(self, event):
+        trans_id = event.get('message').get('trans_id')
         wallet = Wallet.objects.get(id=self.wallet_id)
+        transaction = WalletTransaction.objects.get(id=trans_id)
         if wallet:
+            wallet = wallet.to_json()
+            wallet['transaction'] = transaction.to_json()
             self.send(text_data=json.dumps(wallet.to_json()))
         else:
             self.send(text_data='Không tìm thấy giao dịch')
