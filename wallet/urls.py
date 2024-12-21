378984158c472d3dfbf7de47f4256ba5e07a728d
longIdt2502 <long.lbh@idtinc.co>
2024-12-21 09:25:56 +0700

    wallet: api transaction

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('detail/<uuid:pk>', WalletDetail.as_view()),
     path('payment', WalletPayment.as_view()),
+    path('transaction', WalletTransApi.as_view()),
     path('receive-hook-payment', WalletReceiveHookPayment.as_view()),
 ]
