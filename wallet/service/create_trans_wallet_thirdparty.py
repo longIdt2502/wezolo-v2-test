8bea407bf5444edd413c47c4048008b4d19d0274
longIdt2502 <long.lbh@idtinc.co>
2024-12-24 11:00:20 +0700

    update api

--- wallet/service/create_trans_wallet_thirdparty.py
+++ wallet/service/create_trans_wallet_thirdparty.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from jwt import JWT
+import jwt
 
 def create_transaction_third_party(wallet_trans, metadata):
     from wallet.models import WalletTransaction
@@ -22,7 +22,7 @@ def create_transaction_third_party(wallet_trans, metadata):
     if not wallet.wallet_authorization:
         public_key_bytes = wallet.private_key.encode()
         payload = {"uid": wallet.wallet_uid}
-        encode_string = JWT().encode(payload, key=public_key_bytes, algorithm='RS256')
+        encode_string = jwt.encode(payload, key=public_key_bytes, algorithm='RS256')
         wallet.wallet_authorization = encode_string
         wallet.save()
     headers = {
