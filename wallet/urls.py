17e64d909ece64f7f2cc83e7150c522a62195820
longIdt2502 <long.lbh@idtinc.co>
2024-12-24 14:41:09 +0700

    update api

--- wallet/urls.py
+++ wallet/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('detail/<uuid:pk>', WalletDetail.as_view()),
     path('payment', WalletPayment.as_view()),
     path('transaction', WalletTransApi.as_view()),
+    path('admin/transaction-by-user/<int:pk>', WalletTransAdminApi.as_view()),
     path('receive-hook-payment', WalletReceiveHookPayment.as_view()),
 ]
